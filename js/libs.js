6cb086af14ea3aec7a73205e32cfaaae1d9995db
Jim Myhrberg <contact@jimeh.me>
2015-07-18 15:09:32 +0100

    Get rid of various JS libs I won't need anymore

--- js/libs.js
+++ js/libs.js
@@ -2,7 +2,4 @@
 ---
 {% include js/lib/underscore.min.js %}
 {% include js/lib/backbone.min.js %}
-{% include js/lib/backbone-support.min.js %}
-{% include js/lib/moment.min.js %}
-{% include js/lib/Autolinker.min.js %}
 {% include js/lib/velocity.min.js %}
